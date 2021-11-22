698c132d3904c793b9ff8f479cb502dc4738c3f9
jiangzitao <1291368109@qq.com>
2021-11-22 19:31:47 +0800

    修改版

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-function ajax() {
+//获取商品数据
+(function() {
     let data = null;
     let xmlReq = new XMLHttpRequest;
     xmlReq.open("GET", "json/phoneList.json", false);
@@ -8,19 +9,11 @@ function ajax() {
         }
     };
     xmlReq.send();
-    return JSON.parse(data);
-}
-let ajaxDate = ajax()
-
-function putData() {
-    let getData = ajaxDate;
-    console.log(getData instanceof Array);
-
     let carlist = document.querySelector(".carlist");
     let arr = [];
-    getData.forEach((item) => {
+    JSON.parse(data).forEach((item) => {
         arr.push(`
-    <div class="col mb-3">
+    <div class="col mb-3 " data-price='${item.price}'  data-putData='${item.putData}' data-evalNum='${item.evalNum}'>
                   <div class="card v-100">
                       <img src="${item.imgUrl}" class="card-img-top" alt="...">
                       <div class="card-body">
@@ -39,41 +32,46 @@ function putData() {
     `);
     });
     carlist.innerHTML = arr.join("");
+})()
+
+let priceSortEl = document.querySelector('.priceSort')
+
+let carlist = document.querySelector('.carlist')
+
+let dataList = document.querySelectorAll('.col')
+
+dataList = Array.prototype.slice.call(dataList, 0)
+
+
+priceSortEl.onclick = function() {
+    this['priceSortStatu'] = this['priceSortStatu'] ? this['priceSortStatu'] : -1
+    this['priceSortStatu'] *= -1
+    dataList.sort((m, n) => {
+            return this['priceSortStatu'] * (m.getAttribute("data-price") - n.getAttribute("data-price"))
+        })
+        //往元素内末尾添加排序后的元素,利用了dome的映射机制
+    dataList.forEach(item => carlist.appendChild(item))
 }
-putData();
 let putDateEl = document.querySelector('.putDate')
 putDateEl.onclick = function() {
     this['clickStatu'] = this['clickStatu'] ? this['clickStatu'] : -1
     this['clickStatu'] *= -1
-    ajaxDate.sort((m, n) => {
-        return this['clickStatu'] * (m.putData.replace(/-/g, '') - n.putData.replace(/-/g, ''))
-    })
-    putData()
+    dataList.sort((m, n) => {
+            return this['clickStatu'] * (m.getAttribute("data-putData").replace(/-/g, '') - n.getAttribute("data-putData").replace(/-/g, ''))
+        })
+        //往元素内末尾添加排序后的元素,利用了dome的映射机制
+    dataList.forEach(item => carlist.appendChild(item))
 
-}
-let priceSortEl = document.querySelector('.priceSort')
-let carlist = document.querySelector('.carlist')
-
-
-let dataList = document.querySelectorAll('.col')
-dataList = Array.prototype.slice.call(dataList, 0)
-priceSortEl.onclick = function() {
-    this['priceSortStatu'] = this['priceSortStatu'] ? this['priceSortStatu'] : -1
-    this['priceSortStatu'] *= -1
-    ajaxDate.sort((m, n) => {
-        return this['priceSortStatu'] * (m.price - n.price)
-    })
-
-    putData()
 }
 let hotSortEL = document.querySelector('.hotSort')
 
 hotSortEL.onclick = function() {
     this['hotSortStatu'] = this['hotSortStatu'] ? this['hotSortStatu'] : -1
     this['hotSortStatu'] *= -1
-    ajaxDate.sort((m, n) => {
-        return this['hotSortStatu'] * (m.evalNum - n.evalNum)
+    dataList.sort((m, n) => {
+        return this['hotSortStatu'] * (m.getAttribute("data-evalNum") - n.getAttribute("data-evalNum"))
     })
 
-    putData()
+    //往元素内末尾添加排序后的元素,利用了dome的映射机制
+    dataList.forEach(item => carlist.appendChild(item))
 }
